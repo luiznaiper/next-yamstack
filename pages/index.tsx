fbf9ada47cc67787fc364a02bf345ace2752766c
luiznaiper <luiz_naiper@hotmail.com>
2022-10-17 07:16:40 -0500

    :construction: Add intex.tsx main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
-import { useEffect } from 'react'
-import { Grid } from '@ui/Grid'
-import { Button } from '@ui/Button'
-import { Typography } from '@ui/Typography'
+import { useState, useEffect } from 'react'
+// import { Grid } from '@ui/Grid'
+// import { Button } from '@ui/Button'
+// import { Typography } from '@ui/Typography'
 import { Layout } from '@components/Layout'
+import { PlantCollection } from '@components/PlantCollection'
 import { getPlantList } from '@api'
 
 export default function Home() {
+  const [data, setData] = useState<Plant[]>([])
   useEffect(() => {
-    getPlantList({ limit: 10 }).then((data) => {
-      data.forEach((item) => {
-        console.log(item.author.fullName)
-      })
-    })
+    getPlantList({ limit: 10 }).then((reveivedData) => setData(reveivedData))
   }, [])
-  return <Layout>Hola</Layout>
+
+  console.log(data)
+
+  return (
+    <Layout>
+      <PlantCollection plants={data} variant="square" />
+    </Layout>
+  )
 }
