20e98833544f7468e19292e16bd58599e357d7bf
luiznaiper <luiz_naiper@hotmail.com>
2022-11-09 08:46:53 -0600

    :passport_control: Add next-auth and configure a hardcoded pass

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,37 @@
+import NextAuth from 'next-auth'
+import type { NextAuthOptions } from 'next-auth'
+import Providers from 'next-auth/providers'
+
+const options: NextAuthOptions = {
+  theme: 'light',
+  debug: true,
+  session: {},
+  jwt: {},
+  providers: [
+    Providers.Credentials({
+      name: 'Naiper',
+      credentials: {
+        password: {
+          type: 'password',
+          label: 'Keep programming!',
+        },
+      },
+      async authorize(credentials) {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/naiper`, {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { 'Content-type': 'application/json' },
+        })
+
+        const user = await res.json()
+
+        if (res.ok && user) {
+          return user
+        }
+        return null
+      },
+    }),
+  ],
+}
+
+export default NextAuth(options)
---
+++ pages/api/auth/naiper.ts
@@ -0,0 +1,21 @@
+import type { NextApiHandler } from 'next'
+
+const credentialsAuth: NextApiHandler<User> = (request, response) => {
+  if (request.method !== 'POST') {
+    response.status(405).end()
+    return
+  }
+
+  if (request.body.password === process.env.AUTH_NAIPER_SECRET) {
+    const naiperUser: User = {
+      name: 'Luiznaiper',
+      email: 'luis@luis.com',
+      image: '',
+    }
+    return response.status(200).json(naiperUser)
+  }
+
+  response.status(404).end()
+}
+
+export default credentialsAuth
